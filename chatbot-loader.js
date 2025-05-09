a4a95a876605e9e34d3ec86f0a4e777f8ab0e94c
Aarthi A <aarthi.adhikesavan@gmail.com>
2025-05-09 14:19:10 +0530

    set br-10px in iframe

--- chatbot-loader.js
+++ chatbot-loader.js
@@ -2,12 +2,13 @@
   var iframe = document.createElement("iframe");
   iframe.src = "https://quickassist-bot.netlify.app"; // hosted chatbot app
   iframe.style.position = "fixed";
-  iframe.style.bottom = "20px";
-  iframe.style.right = "20px";
+  iframe.style.bottom = "10px";
+  iframe.style.right = "10px";
   iframe.style.width = "390px";
   iframe.style.height = "670px";
   iframe.style.border = "none";
   iframe.style.zIndex = "9999";
+  iframe.style.backgroundColor = "transparent";
   iframe.allow = "microphone; camera";
   document.body.appendChild(iframe);
 })();
